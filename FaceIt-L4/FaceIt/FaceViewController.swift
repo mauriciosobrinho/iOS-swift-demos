fa0e429f30de424768eeec4a9068c893018cd148
mauriciosobrinho <mauricio@papaleguas.local>
2017-05-23 19:56:35 -0300

    add FaceIt-L4

---
+++ FaceIt-L4/FaceIt/FaceViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Mauricio Luiz Sobrinho on 06/04/17.
+//  Copyright © 2017 Mauricio Luiz Sobrinho. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController
+{
+}
